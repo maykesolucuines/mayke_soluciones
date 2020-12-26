57beb8bd970228b97a26ae438ce898789b746a3a
maykesolucuines <mauricio.mercados@pca.edu.co>
2020-12-25 19:54:25 -0500

    mqtt

--- mayke/html/scripts/conexionmqtt.js
+++ mayke/html/scripts/conexionmqtt.js
@@ -65,3 +65,133 @@ function enviarDatoMQTTbt2() {
     console.log(error || 'Publicacion Exitosa')
   })
 }
+
+function sw1_change(){
+  if ($('#display_sw1').is(":checked"))
+  {
+    client.publish(topic_raiz + '/actions/sw1',"1",{ qos: 0, rein: false }, (error)=> {
+    //console.log(error || 'Mensaje sw1 "1"')
+  })
+  }else{
+    client.publish(topic_raiz + '/actions/sw1',"0",{ qos: 0, rein: false }, (error)=> {
+    //console.log(error || 'Mensaje sw1 "0"')
+   })
+  }
+}
+
+function sw2_change(){
+  if ($('#display_sw2').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw2',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw2',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw3_change(){
+  if ($('#display_sw3').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw3',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw3',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw4_change(){
+  if ($('#display_sw4').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw4',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw4',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw5_change(){
+  if ($('#display_sw5').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw5',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw5',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw6_change(){
+  if ($('#display_sw6').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw6',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw6',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw7_change(){
+  if ($('#display_sw7').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw7',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw7',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw8_change(){
+  if ($('#display_sw8').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw8',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw8',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw9_change(){
+  if ($('#display_sw9').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw9',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw9',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
+
+function sw10_change(){
+  if ($('#display_sw10').is(":checked"))
+  {
+  client.publish(topic_raiz + '/actions/sw10',"1",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "1"')
+})
+}else{
+  client.publish(topic_raiz + '/actions/sw10',"0",{ qos: 0, rein: false }, (error)=> {
+  //console.log(error || 'Mensaje sw2 "0"')
+   })
+  }
+}
